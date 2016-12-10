20fa15c9d5486b363ce014df79696b84f4da9466
Keiho Sakapon <git@saka-pon.net>
2016-12-10 17:31:40 +0900

    Add tools to create package

---
+++ Tools/CreatePackage.ps1
@@ -0,0 +1,15 @@
+﻿# $msbuild = "C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
+$msbuild = "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe"
+
+.\IncrementVersion-cs.ps1 ..\Blaze\Blaze
+
+$slnFilePath = "..\Blaze\Blaze.sln"
+& $msbuild $slnFilePath /p:Configuration=Release /t:Clean
+& $msbuild $slnFilePath /p:Configuration=Release /t:Rebuild
+
+cd ..\Blaze\Blaze
+.\NuGetPackup.exe
+
+ni ..\..\Published -type directory -Force
+move *.nupkg ..\..\Published -Force
+explorer ..\..\Published
