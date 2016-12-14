26b72d9b70ef5db4c525db81faead21efae8bb29
Keiho Sakapon <git@saka-pon.net>
2016-12-14 18:14:51 +0900

    Add test

--- Blaze/Blaze/Properties/AssemblyInfo.cs
+++ Blaze/Blaze/Properties/AssemblyInfo.cs
@@ -37,8 +37,8 @@ using System.Runtime.InteropServices;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1")]
+[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.0.2")]
 
 [assembly: CLSCompliant(true)]
 [assembly: InternalsVisibleTo("UnitTest")]
