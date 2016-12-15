f7e33d05ca3281d83b518e8ce32890f6648e13a4
Keiho Sakapon <git@saka-pon.net>
2016-12-15 16:53:08 +0900

    Add classes for formulas

---
+++ Blaze/Blaze/Propositions/Formula.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Blaze.Propositions
+{
+    public abstract class Formula
+    {
+        internal static Formula[] EmptyFormulas { get; } = new Formula[0];
+
+        public abstract Formula[] Children { get; }
+        public abstract bool? IsTrue { get; }
+    }
+}
---
+++ Blaze/Blaze/Propositions/FormulaClasses.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Blaze.Propositions
+{
+    public class ConstantFormula : Formula
+    {
+        public override Formula[] Children => EmptyFormulas;
+        public override bool? IsTrue => Value;
+
+        public bool Value { get; }
+
+        public ConstantFormula(bool value)
+        {
+            Value = value;
+        }
+
+        public override string ToString() => Value ? "t" : "f";
+    }
+
+    public abstract class UnaryFormula : Formula
+    {
+        public override Formula[] Children { get; }
+
+        public Formula Operand { get; }
+
+        protected UnaryFormula(Formula operand)
+        {
+            Operand = operand;
+            Children = new[] { Operand };
+        }
+    }
+
+    public abstract class BinaryFormula : Formula
+    {
+        public override Formula[] Children { get; }
+
+        public Formula Operand1 { get; }
+        public Formula Operand2 { get; }
+
+        protected BinaryFormula(Formula operand1, Formula operand2)
+        {
+            Operand1 = operand1;
+            Operand2 = operand2;
+            Children = new[] { Operand1, Operand2 };
+        }
+    }
+
+    public class NegationFormula : UnaryFormula
+    {
+        public override bool? IsTrue => !Operand.IsTrue;
+
+        public NegationFormula(Formula operand) : base(operand) { }
+
+        public override string ToString() => $"～({Operand})";
+    }
+
+    public class AndFormula : BinaryFormula
+    {
+        public override bool? IsTrue => Operand1.IsTrue & Operand2.IsTrue;
+
+        public AndFormula(Formula operand1, Formula operand2) : base(operand1, operand2) { }
+
+        public override string ToString() => $"({Operand1})∧({Operand2})";
+    }
+
+    public class OrFormula : BinaryFormula
+    {
+        public override bool? IsTrue => Operand1.IsTrue | Operand2.IsTrue;
+
+        public OrFormula(Formula operand1, Formula operand2) : base(operand1, operand2) { }
+
+        public override string ToString() => $"({Operand1})∨({Operand2})";
+    }
+
+    public class ImplicationFormula : BinaryFormula
+    {
+        public override bool? IsTrue => !Operand1.IsTrue | Operand2.IsTrue;
+
+        public ImplicationFormula(Formula operand1, Formula operand2) : base(operand1, operand2) { }
+
+        public override string ToString() => $"({Operand1})⇒({Operand2})";
+    }
+
+    public class EquivalenceFormula : BinaryFormula
+    {
+        public override bool? IsTrue => !(Operand1.IsTrue ^ Operand2.IsTrue);
+
+        public EquivalenceFormula(Formula operand1, Formula operand2) : base(operand1, operand2) { }
+
+        public override string ToString() => $"({Operand1})≡({Operand2})";
+    }
+
+    public class XorFormula : BinaryFormula
+    {
+        public override bool? IsTrue => Operand1.IsTrue ^ Operand2.IsTrue;
+
+        public XorFormula(Formula operand1, Formula operand2) : base(operand1, operand2) { }
+
+        public override string ToString() => $"({Operand1})≢({Operand2})";
+    }
+}
