14cfcb6d6f13b3e1a6bc319cf3967ae9eba3a082
Keiho Sakapon <git@saka-pon.net>
2016-12-15 17:40:13 +0900

    Add static methods

--- Blaze/Blaze/Propositions/Formula.cs
+++ Blaze/Blaze/Propositions/Formula.cs
@@ -6,9 +6,25 @@ namespace Blaze.Propositions
 {
     public abstract class Formula
     {
-        internal static Formula[] EmptyFormulas { get; } = new Formula[0];
-
         public abstract Formula[] Children { get; }
         public abstract bool? IsTrue { get; }
+
+        internal static Formula[] EmptyFormulas { get; } = new Formula[0];
+
+        public static Formula True { get; } = new ConstantFormula(true);
+        public static Formula False { get; } = new ConstantFormula(false);
+
+        public static Formula Variable<TStatement>(TStatement statement) => new VariableFormula<TStatement>(statement);
+        public static Formula Imply(Formula v1, Formula v2) => new ImplicationFormula(v1, v2);
+        public static Formula Equivalent(Formula v1, Formula v2) => new EquivalenceFormula(v1, v2);
+
+        public static Formula And(IEnumerable<Formula> formulas) => formulas.Aggregate((v1, v2) => v1 & v2);
+        public static Formula Or(IEnumerable<Formula> formulas) => formulas.Aggregate((v1, v2) => v1 | v2);
+        public static Formula Xor(IEnumerable<Formula> formulas) => formulas.Aggregate((v1, v2) => v1 ^ v2);
+
+        public static Formula operator !(Formula v) => new NegationFormula(v);
+        public static Formula operator &(Formula v1, Formula v2) => new AndFormula(v1, v2);
+        public static Formula operator |(Formula v1, Formula v2) => new OrFormula(v1, v2);
+        public static Formula operator ^(Formula v1, Formula v2) => new XorFormula(v1, v2);
     }
 }
