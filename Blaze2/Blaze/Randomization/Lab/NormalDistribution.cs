e91152019c76b12768ad8a7248ff4efef5bd8bbf
Keiho Sakapon <git@saka-pon.net>
2019-02-03 00:36:33 +0900

    Add classes for normal distribution

---
+++ Blaze2/Blaze/Randomization/Lab/NormalDistribution.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Blaze.Randomization.Lab
+{
+    public static class NormalDistribution
+    {
+        static readonly Random random = new Random();
+
+        public static double NextDouble()
+        {
+            var x = random.NextDouble();
+            var y = random.NextDouble();
+
+            return Math.Sqrt(-2 * Math.Log(x)) * Math.Sin(2 * Math.PI * y);
+        }
+    }
+}
---
+++ Blaze2/UnitTest/Randomization/Lab/NormalDistributionTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Blaze.Randomization.Lab;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest.Randomization.Lab
+{
+    [TestClass]
+    public class NormalDistributionTest
+    {
+        [TestMethod]
+        public void NextDouble()
+        {
+            for (var i = 0; i < 100; i++)
+                Console.WriteLine(NormalDistribution.NextDouble());
+        }
+
+        [TestMethod]
+        public void NextDouble_Rare()
+        {
+            for (var i = 0; i < 10000; i++)
+            {
+                var x = NormalDistribution.NextDouble();
+                if (Math.Abs(x) > 3)
+                    Console.WriteLine(x);
+            }
+        }
+    }
+}
