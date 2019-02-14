1829be395e437d81474907409e896c2ffc5fb032
Keiho Sakapon <git@saka-pon.net>
2019-02-14 11:39:40 +0900

    Add classes

---
+++ Blaze2/Blaze/Randomization/Lab/BinomialDistribution.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using static System.Math;
+using static Blaze.Randomization.Lab.NormalDistribution;
+
+namespace Blaze.Randomization.Lab
+{
+    public static class BinomialDistribution
+    {
+        // -M <= x <= M
+        // 正規分布で近似します。
+        public static int NextInt32(int maxAbsValue)
+        {
+            var sigma = Sqrt(2 * maxAbsValue) / 2.0;
+
+            var x = Truncate(maxAbsValue + 0.5, sigma);
+            return (int)Round(x, MidpointRounding.AwayFromZero);
+        }
+
+        // m <= x <= M
+        public static int NextInt32ByMinMax(int minValue, int maxValue)
+        {
+            var mean = (maxValue + minValue) / 2.0;
+            var sigma = Sqrt(maxValue - minValue) / 2.0;
+            var maxAbsValue = (maxValue - minValue) / 2.0 + 0.5;
+
+            var x = Truncate(maxAbsValue, sigma);
+            return (int)Round(x + mean, MidpointRounding.AwayFromZero);
+        }
+    }
+}
---
+++ Blaze2/UnitTest/Randomization/Lab/BinomialDistributionTest.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Blaze.Randomization.Lab;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static System.Math;
+
+namespace UnitTest.Randomization.Lab
+{
+    [TestClass]
+    public class BinomialDistributionTest
+    {
+        [TestMethod]
+        public void NextInt32()
+        {
+            var M = 3;
+
+            for (var i = 0; i < 10000; i++)
+            {
+                var x = BinomialDistribution.NextInt32(M);
+                Assert.IsTrue(Abs(x) <= M);
+            }
+
+            var values = Enumerable.Repeat(false, 100)
+                .Select(_ => BinomialDistribution.NextInt32(M))
+                .OrderBy(x => x);
+            foreach (var x in values)
+                Console.WriteLine(x);
+        }
+
+        [TestMethod]
+        public void NextInt32ByMinMax()
+        {
+            var m = 11;
+            var M = 16;
+
+            for (var i = 0; i < 10000; i++)
+            {
+                var x = BinomialDistribution.NextInt32ByMinMax(m, M);
+                Assert.IsTrue(m <= x && x <= M);
+            }
+
+            var values = Enumerable.Repeat(false, 100)
+                .Select(_ => BinomialDistribution.NextInt32ByMinMax(m, M))
+                .OrderBy(x => x);
+            foreach (var x in values)
+                Console.WriteLine(x);
+        }
+
+        public static long Permutation(int n, int k)
+        {
+            if (k == 0) return 1;
+            return Enumerable.Range(0, k).Select(i => n - i).Aggregate(1L, (x0, x) => x0 * x);
+        }
+
+        public static long Combination(int n, int k)
+        {
+            if (k > n / 2) k = n - k;
+            if (k == 0) return 1;
+            return Permutation(n, k) / Enumerable.Range(1, k).Aggregate(1L, (x0, x) => x0 * x);
+        }
+    }
+}
