5eb96111a9884be4f1df48782662a26ec664bbad
Keiho Sakapon <git@saka-pon.net>
2019-06-14 18:11:02 +0900

    Add class for exponential distribution

---
+++ Blaze2/Blaze/Randomization/Lab/ExponentialDistribution.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Blaze.Randomization.Lab
+{
+    public static class ExponentialDistribution
+    {
+        static readonly Random random = new Random();
+
+        public static double Next(double mean) =>
+            -mean * Math.Log(1 - random.NextDouble());
+    }
+}
