ea107f25912f19c6f190cc91d0e9c33f81c21987
Keiho Sakapon <git@saka-pon.net>
2019-06-23 20:42:36 +0900

    Add class for binary search

---
+++ Blaze2/Blaze/Randomization/Lab/BinarySearch.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Blaze.Randomization.Lab
+{
+    public static class BinarySearch
+    {
+        static readonly Random random = new Random();
+
+        // 累積分布関数 (単調増加数列) であることを前提とします。
+        // a[i] <= x < a[i+1] のとき、i を返します。
+        public static int GetIndexByRange(this double[] array, double value)
+        {
+            if (array == null) throw new ArgumentNullException(nameof(array));
+            if (array.Length == 0) return -1;
+
+            return Search(0, array.Length);
+
+            int Search(int start, int count)
+            {
+                if (count == 1)
+                    return array[start] <= value ? start : -1;
+
+                var c1 = count >> 1;
+                var s2 = start + c1;
+                return value < array[s2] ? Search(start, c1) : Search(s2, count - c1);
+            }
+        }
+
+        // 累積分布関数 (単調増加数列) であることを前提とします。
+        // 最初の要素は 0。最後の要素は 1 未満でも 1 以上でも可。
+        public static int GetRandomIndexByCumulation(this double[] cumulation)
+        {
+            var v = random.NextDouble();
+            return GetIndexByRange(cumulation, v);
+        }
+    }
+}
---
+++ Blaze2/UnitTest/Randomization/Lab/BinarySearchTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Blaze.Randomization.Lab;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest.Randomization.Lab
+{
+    [TestClass]
+    public class BinarySearchTest
+    {
+        static readonly Random random = new Random();
+
+        [TestMethod]
+        public void GetRandomIndexByCumulation_1()
+        {
+            var count = 99999;
+            var mean = count / 2.0;
+
+            var probs = Enumerable.Range(0, count)
+                .Select(i => random.NextDouble())
+                .ToArray();
+            var cumulation = new double[count + 1];
+            for (var i = 0; i < count; i++)
+                cumulation[i + 1] = cumulation[i] + probs[i] / mean;
+
+            var result = Enumerable.Range(0, count)
+                .Select(_ => cumulation.GetRandomIndexByCumulation())
+                .GroupBy(i => i)
+                .OrderByDescending(g => g.Count())
+                .ToArray();
+            var summary = result.Take(30)
+                .Concat(result.Skip(result.Length - 30))
+                .ToDictionary(g => g.Key, g => g.Count());
+
+            foreach (var p in summary)
+                Console.WriteLine($"{p.Key}: {p.Value}, {(p.Key == count ? (1 - cumulation[p.Key]) * mean : probs[p.Key])}");
+        }
+    }
+}
